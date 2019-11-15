f8fd93b653571c22674288e00e3dcfcce276c5ca
saydnasr <sayd.nasr@gmail.com>
2019-11-15 13:26:04 +0200

    Update FirebasePlugin.java

--- src/android/FirebasePlugin.java
+++ src/android/FirebasePlugin.java
@@ -396,6 +396,21 @@ public class FirebasePlugin extends CordovaPlugin {
     });
   }
 
+    private void createNotificationChannel() {
+    // Create the NotificationChannel, but only on API 26+ because
+    // the NotificationChannel class is new and not in the support library
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        String description = "Get deal notification";
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Deals", importance);
+        channel.setDescription(description);
+        // Register the channel with the system; you can't change the importance
+        // or other notification behaviors after this
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+    }
+}
+
   private void getBadgeNumber(final CallbackContext callbackContext) {
     Log.d(TAG, "getBadgeNumber called");
     cordova.getThreadPool().execute(new Runnable() {
